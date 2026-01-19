f38d403653e14a9d201d6a82e8bfe2320b2b334e
Workshop Public Cloud <cj-cloud-wave@workshops.aws>
2026-01-19 08:27:00 +0000

    update login function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from streamlit_option_menu import option_menu
 from widget.login import __login__
 from menu.homepage_linux import Homepage
@@ -7,49 +6,57 @@ from menu.photo_gallery import PhotoGallery
 from menu.travel_attraction import TravelAttraction
 
 st.set_page_config(
-    page_title='CJ OliveNetworks CloudWave', 
+    page_title='CJ OliveNetworks CloudWave',
     page_icon="./images/cj-olivenetworks.png",
-    layout="wide", 
-    initial_sidebar_state="expanded", 
+    layout="wide",
+    initial_sidebar_state="expanded",
     menu_items=None
 )
 
+# 로그인 상태 초기화
 if 'logged_in' not in st.session_state:
-    st.session_state['logged_in'] = True
-    # RDS Database Login Session / Login Function
-    # st.session_state['logged_in'] = False
+    st.session_state['logged_in'] = False
+
 
 def side_bar():
     with st.sidebar.container():
         selected_page = option_menu(
-            menu_title="Main Menu", 
-            #options=["Main_Home"],
-             options=["Main_Home", "Photo_Gallery"],
-            # options=["Main_Home", "Photo_Gallery", "Travel_Attractions"],
-            icons=['bi bi-house', 'bi bi-images', 'bi bi-airplane-engines'],
-            menu_icon="cast", 
+            menu_title="Main Menu",
+            options=["Main_Home", "Photo_Gallery"],
+            icons=['bi bi-house', 'bi bi-images'],
+            menu_icon="cast",
             default_index=0,
             styles={
                 "container": {"padding": "4!important", "background-color": "#fafafa"},
                 "icon": {"color": "black", "font-size": "20px"},
-                "nav-link": {"font-size": "14px", "text-align": "left", "margin":"0px", "--hover-color": "#fafafa"},
+                "nav-link": {
+                    "font-size": "14px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "--hover-color": "#fafafa"
+                },
                 "nav-link-selected": {"background-color": "#08c7b4"}
             }
         )
-        # col1, col2, col3 = st.columns(3)
-        # with col1:
-        #     # RDS Database Login Session / Logout Function      
-        #     if st.button('Logout', use_container_width=True):
-        #         st.session_state['logged_in'] = False
-        #         st.rerun()
-        # with col2:
-        #     if st.button('ReRun', use_container_width=True):
-        #         st.rerun()
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            if st.button('Logout', use_container_width=True):
+                st.session_state['logged_in'] = False
+                st.rerun()
+
+        with col2:
+            if st.button('ReRun', use_container_width=True):
+                st.rerun()
+
     return selected_page
 
+
 def main():
-    if st.session_state['logged_in'] == True:
+    if st.session_state['logged_in']:
         selected_page = side_bar()
+
         if selected_page == 'Main_Home':
             Homepage()
         elif selected_page == 'Photo_Gallery':
@@ -58,6 +65,7 @@ def main():
             TravelAttraction()
     else:
         __login__()
-    
+
+
 if __name__ == "__main__":
     main()
